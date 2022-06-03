d99a45362a8936a6740311333fcaa115764e047d
husniddinuzinfocom <105488050+husniddinuzinfocom@users.noreply.github.com>
2022-06-03 17:47:43 +0500

    s

--- js/srcipt.js
+++ js/srcipt.js
@@ -1,35 +1,32 @@
-const li =document.querySelectorAll(".nav__row > li");
-const a =document.querySelectorAll(".nav__row > li > a");
+const li = document.querySelectorAll(".nav__row > li");
+const a = document.querySelectorAll(".nav__row > li > a");
 
-if(li) {
-    for(let x of li) {
-        x.addEventListener("mouseover", (e) => {
-            if(e.target.querySelector(".content")) {
+if (li) {
+    for (let x of li) {
+        x.addEventListener("mouseenter", (e) => {
+            if (e.target.querySelector(".content")) {
                 e.target.querySelector(".content").classList.add("active");
+                e.target.querySelector(".content").style.top = x.getBoundingClientRect().bottom + "px";
             }
         });
         x.addEventListener("mouseout", (e) => {
-            if(e.target.querySelector(".content")) {
-                e.target.querySelector(".content").classList.remove("active");
+            if (e.target.querySelector(".content")) {
+                e.target.querySelector(".content").classList.add("anim");
+                setTimeout(() => {
+                    e.target.querySelector(".content").classList.remove("anim")
+                    e.target.querySelector(".content").classList.remove("active");
+                }, 500);
             }
-        });
-        x.querySelector("a").addEventListener("mouseover", (e) => {
-            e.target.nextElementSibling.nextElementSibling.classList.add("active");
-        });
-        x.querySelector("a").addEventListener("mouseout", (e) => {
-            e.target.nextElementSibling.nextElementSibling.classList.remove("active");
         });
         x.querySelector(".content").addEventListener("mouseover", (e) => {
             e.target.classList.add("active");
         });
         x.querySelector(".content").addEventListener("mouseout", (e) => {
-            e.target.classList.remove("active");
-        });
-        x.querySelector("span").addEventListener("mouseover", (e) => {
-            e.target.nextElementSibling.classList.add("active");
-        });
-        x.querySelector("span").addEventListener("mouseout", (e) => {
-            e.target.nextElementSibling.classList.remove("active");
+            e.target.classList.add("anim");
+            setTimeout(() => {
+                e.target.classList.remove("anim")
+                e.target.classList.remove("active");
+            }, 500);
         });
     }
 }
